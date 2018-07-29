c530674f689586b01e4b9e92b47430e186e036ca
craig-davey96 <craig96@talktalk.net>
2018-07-29 22:00:02 +0100

    Update initpaypal.php

--- internals/initpaypal.php
+++ internals/initpaypal.php
@@ -4,8 +4,8 @@
 
 	$apiContext = new \PayPal\Rest\ApiContext(
 	    new \PayPal\Auth\OAuthTokenCredential(
-	        'AT3R4bWmjIABm-f2SuuwdE1RqIj3_O2NkgSLHSD4Eckq4Yk4Q3f7Nx4QOIhqEskjJ3fc-iAyCXURyeJs',     // ClientID
-	        'EKB6MQ53YULqjGRzDAhpNv2RBGkvO5jFUWgwaV49L5iA99VvCg0dqbh8KakczdBb03ctAC9rVpIcI7N4'      // ClientSecret
+	        '',     // ClientID
+	        ''      // ClientSecret
 	    )
 	);
 
